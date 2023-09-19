c4e9725c36f76bc66135254c39c619331d030b48
RodriCoder <rodric0628>
2023-09-18 22:01:07 -0300

    aeiou

---
+++ js/JavaScript.js
@@ -0,0 +1,6 @@
+document.getElementById("login-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    window.location.href = "../index.html";
+});
