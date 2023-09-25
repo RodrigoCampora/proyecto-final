10074d319a43b09b2878e1834242c4318d3334e3
RodriCoder <rodric0628>
2023-09-25 19:47:34 -0300

    si

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -4,3 +4,23 @@ document.getElementById("login-form").addEventListener("submit", function (e) {
     const password = document.getElementById("password").value;
     window.location.href = "../index.html";
 });
+
+   // Datos de los planes con precios
+   var planes = [
+    {
+        nombre: "Plan Standard",
+        caracteristicas: ["Gratis", "Acceso a diversas herramientas", "Limitado"],
+        precio: "$0/mes"
+    },
+    {
+        nombre: "Plan Pro",
+        caracteristicas: ["Acceso a herramientas profesionales", "Ilimitado", "Guardado en la nube"],
+        precio: "$19.99/mes"
+    },
+    {
+        nombre: "Plan Full",
+        caracteristicas: ["Todas las herramientas"],
+        precio: "$49.99/mes"
+    }
+];
+
